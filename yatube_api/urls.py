69dadb768487c5d5c0c5ef034996841930650810
Михаил <amasls@ya.ru>
2021-05-12 00:23:03 +0400

    before review v.1.0

--- yatube_api/urls.py
+++ yatube_api/urls.py
@@ -12,7 +12,14 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(
+        'api/v1/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'api/v1/token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
 ]
-
